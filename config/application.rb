0c965bc3a126e011549bbf36541e05cb6a55216c
ikeda <ikebright@gmail.com>
2016-10-01 14:59:14 +0900

    initialize Rspec

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,18 @@ module FlowSample
       g.assets     false
       g.helper     false
     end
-    
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
